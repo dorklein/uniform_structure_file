75292e956bcda2d1c5107f5e0dda0cd7e9364328
dorklein <dorklein2@gmail.com>
2023-06-05 11:21:17 +0300

    feat: dont include item rows (D110) in receipts

--- src/generator/dataGenerator.ts
+++ src/generator/dataGenerator.ts
@@ -1,226 +1,231 @@
-import { BaseGenerator } from './baseGenerator'
+import {BaseGenerator} from './baseGenerator'
 import {
-  BKMVDATARowsCount,
-  Cell,
-  DocumentItem,
-  DocumentPayment,
-  DocumentRecord,
-  UniformStructureInput,
+    BKMVDATARowsCount,
+    Cell,
+    DocumentItem,
+    DocumentPayment,
+    DocumentRecord,
+    DocumentType,
+    UniformStructureInput,
 } from '../types'
-import { getNestedValue } from '../utils/objs'
-import { bkmvdataSchema } from '../schemas/bkmvdataSchema'
-import { expectedDATALineLength } from '../utils/constants'
+import {getNestedValue} from '../utils/objs'
+import {bkmvdataSchema} from '../schemas/bkmvdataSchema'
+import {expectedDATALineLength} from '../utils/constants'
 
 export class DataGenerator extends BaseGenerator {
-  constructor(input: UniformStructureInput, uuid: string, filePath: string) {
-    super(input, uuid, filePath)
-  }
-
-  protected validateHeader() {
-    // recordCode
-    const recordCode = 'A100'
-    const line = this.lines[0]
-    if (!line.startsWith(recordCode)) {
-      throw new Error(`BKMVDATA.txt first line should start with ${recordCode}`)
-    }
-  }
-
-  protected validateFooter() {
-    // recordCode
-    const recordCode = 'Z900'
-    const line = this.lines[this.lines.length - 1]
-    if (!line.startsWith(recordCode)) {
-      throw new Error(`BKMVDATA.txt last line should start with ${recordCode}`)
+    constructor(input: UniformStructureInput, uuid: string, filePath: string) {
+        super(input, uuid, filePath)
     }
 
-    // Has the right total records number
-    const expectedTotalRows = this.lines.length
-    const totalRows = parseInt(line.slice(46, 60))
-    if (totalRows !== expectedTotalRows) {
-      throw new Error(
-        `BKMVDATA.txt last line doesn't have the valid total records. Expected ${expectedTotalRows}, actual ${totalRows}`
-      )
-    }
-  }
-
-  private validateLength(): void {
-    for (const line of this.lines) {
-      const recordCode = line.substring(0, 4)
-      const actualLength = line.length
-
-      const expectedLength = expectedDATALineLength[recordCode]
-      if (actualLength !== expectedLength) {
-        throw new Error(
-          `BKMVDATA.txt ${recordCode} line length should be ${expectedLength}. Actual ${line.length}`
-        )
-      }
-    }
-  }
-  private validateRunningNumbers(): void {
-    const existingNumbers = new Set<number>()
-
-    for (const line of this.lines) {
-      const runningNumber = parseInt(line.slice(5, 13))
-      if (existingNumbers.has(runningNumber)) {
-        throw new Error(
-          `BKMVDATA.txt has more than one '${existingNumbers}' running number`
-        )
-      }
-
-      existingNumbers.add(runningNumber)
+    protected validateHeader() {
+        // recordCode
+        const recordCode = 'A100'
+        const line = this.lines[0]
+        if (!line.startsWith(recordCode)) {
+            throw new Error(`BKMVDATA.txt first line should start with ${recordCode}`)
+        }
     }
 
-    // All numbers must be sequential
-    if (existingNumbers.size !== this.lines.length) {
-      throw new Error(`BKMVDATA.txt has skipped some running numbers`)
-    }
-  }
+    protected validateFooter() {
+        // recordCode
+        const recordCode = 'Z900'
+        const line = this.lines[this.lines.length - 1]
+        if (!line.startsWith(recordCode)) {
+            throw new Error(`BKMVDATA.txt last line should start with ${recordCode}`)
+        }
 
-  protected validateBeforeSave(): void {
-    // Validate first is header
-    this.validateHeader()
-
-    // Validate last is footer
-    this.validateFooter()
-
-    // Validate line length
-    this.validateLength()
-
-    // Validate running numbers
-    this.validateRunningNumbers()
-  }
-
-  public countRowsByType(): BKMVDATARowsCount {
-    // This should be called only after all rows were created
-    this.validateFooter()
-
-    return {
-      C100: this.lines.filter((value) => value.startsWith('C100')).length,
-      D110: this.lines.filter((value) => value.startsWith('D110')).length,
-      D120: this.lines.filter((value) => value.startsWith('D120')).length,
-      B100: this.lines.filter((value) => value.startsWith('B100')).length,
-      B110: this.lines.filter((value) => value.startsWith('B110')).length,
-      M100: this.lines.filter((value) => value.startsWith('M100')).length,
-    }
-  }
-
-  private createBKMVDATAHeader(): void {
-    const getValue = (cell: Cell) => {
-      return (
-        this.computedValues(cell) ??
-        getNestedValue(this.input, cell.name) ??
-        cell.default
-      )
+        // Has the right total records number
+        const expectedTotalRows = this.lines.length
+        const totalRows = parseInt(line.slice(46, 60))
+        if (totalRows !== expectedTotalRows) {
+            throw new Error(
+                `BKMVDATA.txt last line doesn't have the valid total records. Expected ${expectedTotalRows}, actual ${totalRows}`
+            )
+        }
     }
 
-    this.createRow(bkmvdataSchema.header, getValue, undefined)
-  }
+    private validateLength(): void {
+        for (const line of this.lines) {
+            const recordCode = line.substring(0, 4)
+            const actualLength = line.length
 
-  private createC100Row(document: DocumentRecord): void {
-    const getValue = (cell: Cell<DocumentRecord>) => {
-      return (
-        this.computedValues(cell) ??
-        getNestedValue(document, cell.name) ??
-        getNestedValue(this.input, cell.name) ??
-        cell.default
-      )
+            const expectedLength = expectedDATALineLength[recordCode]
+            if (actualLength !== expectedLength) {
+                throw new Error(
+                    `BKMVDATA.txt ${recordCode} line length should be ${expectedLength}. Actual ${line.length}`
+                )
+            }
+        }
     }
 
-    this.createRow<DocumentRecord>(bkmvdataSchema.C100, getValue, document)
-  }
+    private validateRunningNumbers(): void {
+        const existingNumbers = new Set<number>()
 
-  private createD110Row(document: DocumentRecord, item: DocumentItem, index: number): void {
-    const getValue = (cell: Cell<DocumentItem>) => {
-      if (cell.name === 'lineNumber') {
-        return index + 1
-      }
+        for (const line of this.lines) {
+            const runningNumber = parseInt(line.slice(5, 13))
+            if (existingNumbers.has(runningNumber)) {
+                throw new Error(
+                    `BKMVDATA.txt has more than one '${existingNumbers}' running number`
+                )
+            }
 
-      return (
-        this.computedValues(cell) ??
-        getNestedValue(item, cell.name) ??
-        getNestedValue(document, cell.name) ??
-        getNestedValue(this.input, cell.name) ??
-        cell.default
-      )
+            existingNumbers.add(runningNumber)
+        }
+
+        // All numbers must be sequential
+        if (existingNumbers.size !== this.lines.length) {
+            throw new Error(`BKMVDATA.txt has skipped some running numbers`)
+        }
     }
 
-    this.createRow<DocumentItem>(bkmvdataSchema.D110, getValue, item)
-  }
+    protected validateBeforeSave(): void {
+        // Validate first is header
+        this.validateHeader()
 
-  private createD120Row(document: DocumentRecord, item: DocumentPayment, index: number): void {
-    const getValue = (cell: Cell<DocumentPayment>) => {
-      if (cell.name === 'lineNumber') {
-        return index + 1
-      }
+        // Validate last is footer
+        this.validateFooter()
 
-      return (
-        this.computedValues(cell) ??
-        getNestedValue(item, cell.name) ??
-        getNestedValue(document, cell.name) ??
-        getNestedValue(this.input, cell.name) ??
-        cell.default
-      )
+        // Validate line length
+        this.validateLength()
+
+        // Validate running numbers
+        this.validateRunningNumbers()
     }
 
-    this.createRow<DocumentPayment>(bkmvdataSchema.D120, getValue, item)
-  }
+    public countRowsByType(): BKMVDATARowsCount {
+        // This should be called only after all rows were created
+        this.validateFooter()
 
-  // private createB100Row(document: DocumentRecord, item: DocumentPayment): void {
-  //   const getValue = (cell: Cell<DocumentPayment>) => {
-  //     return (
-  //       this.computedValues(cell) ??
-  //       getNestedValue(item, cell.name) ??
-  //       getNestedValue(document, cell.name) ??
-  //       getNestedValue(this.input, cell.name) ??
-  //       cell.default
-  //     )
-  //   }
-  //
-  //   this.createRow<DocumentPayment>(bkmvdataSchema.B100, getValue, item)
-  // }
-  //
-  // private createB110Row(document: DocumentRecord, item: DocumentPayment): void {
-  //   const getValue = (cell: Cell<DocumentPayment>) => {
-  //     return (
-  //       this.computedValues(cell) ??
-  //       getNestedValue(item, cell.name) ??
-  //       getNestedValue(document, cell.name) ??
-  //       getNestedValue(this.input, cell.name) ??
-  //       cell.default
-  //     )
-  //   }
-  //
-  //   this.createRow<DocumentPayment>(bkmvdataSchema.B110, getValue, item)
-  // }
-
-  private createBKMVDATABody(): void {
-    for (const document of this.input.documents) {
-      this.createC100Row(document)
-
-      document.items.forEach((item, index) => {
-        this.createD110Row(document, item, index)
-      })
-      document.payments.forEach((payment, index) => {
-        this.createD120Row(document, payment, index)
-      })
-    }
-  }
-
-  private createBKMVDATAFooter() {
-    const getValue = (cell: Cell) => {
-      return (
-        this.computedValues(cell) ??
-        getNestedValue(this.input, cell.name) ??
-        cell.default
-      )
+        return {
+            C100: this.lines.filter((value) => value.startsWith('C100')).length,
+            D110: this.lines.filter((value) => value.startsWith('D110')).length,
+            D120: this.lines.filter((value) => value.startsWith('D120')).length,
+            B100: this.lines.filter((value) => value.startsWith('B100')).length,
+            B110: this.lines.filter((value) => value.startsWith('B110')).length,
+            M100: this.lines.filter((value) => value.startsWith('M100')).length,
+        }
     }
 
-    this.createRow(bkmvdataSchema.footer, getValue, undefined)
-  }
+    private createBKMVDATAHeader(): void {
+        const getValue = (cell: Cell) => {
+            return (
+                this.computedValues(cell) ??
+                getNestedValue(this.input, cell.name) ??
+                cell.default
+            )
+        }
 
-  protected create() {
-    this.createBKMVDATAHeader()
-    this.createBKMVDATABody()
-    this.createBKMVDATAFooter()
-  }
+        this.createRow(bkmvdataSchema.header, getValue, undefined)
+    }
+
+    private createC100Row(document: DocumentRecord): void {
+        const getValue = (cell: Cell<DocumentRecord>) => {
+            return (
+                this.computedValues(cell) ??
+                getNestedValue(document, cell.name) ??
+                getNestedValue(this.input, cell.name) ??
+                cell.default
+            )
+        }
+
+        this.createRow<DocumentRecord>(bkmvdataSchema.C100, getValue, document)
+    }
+
+    private createD110Row(document: DocumentRecord, item: DocumentItem, index: number): void {
+        const getValue = (cell: Cell<DocumentItem>) => {
+            if (cell.name === 'lineNumber') {
+                return index + 1
+            }
+
+            return (
+                this.computedValues(cell) ??
+                getNestedValue(item, cell.name) ??
+                getNestedValue(document, cell.name) ??
+                getNestedValue(this.input, cell.name) ??
+                cell.default
+            )
+        }
+
+        this.createRow<DocumentItem>(bkmvdataSchema.D110, getValue, item)
+    }
+
+    private createD120Row(document: DocumentRecord, item: DocumentPayment, index: number): void {
+        const getValue = (cell: Cell<DocumentPayment>) => {
+            if (cell.name === 'lineNumber') {
+                return index + 1
+            }
+
+            return (
+                this.computedValues(cell) ??
+                getNestedValue(item, cell.name) ??
+                getNestedValue(document, cell.name) ??
+                getNestedValue(this.input, cell.name) ??
+                cell.default
+            )
+        }
+
+        this.createRow<DocumentPayment>(bkmvdataSchema.D120, getValue, item)
+    }
+
+    // private createB100Row(document: DocumentRecord, item: DocumentPayment): void {
+    //   const getValue = (cell: Cell<DocumentPayment>) => {
+    //     return (
+    //       this.computedValues(cell) ??
+    //       getNestedValue(item, cell.name) ??
+    //       getNestedValue(document, cell.name) ??
+    //       getNestedValue(this.input, cell.name) ??
+    //       cell.default
+    //     )
+    //   }
+    //
+    //   this.createRow<DocumentPayment>(bkmvdataSchema.B100, getValue, item)
+    // }
+    //
+    // private createB110Row(document: DocumentRecord, item: DocumentPayment): void {
+    //   const getValue = (cell: Cell<DocumentPayment>) => {
+    //     return (
+    //       this.computedValues(cell) ??
+    //       getNestedValue(item, cell.name) ??
+    //       getNestedValue(document, cell.name) ??
+    //       getNestedValue(this.input, cell.name) ??
+    //       cell.default
+    //     )
+    //   }
+    //
+    //   this.createRow<DocumentPayment>(bkmvdataSchema.B110, getValue, item)
+    // }
+
+    private createBKMVDATABody(): void {
+        for (const document of this.input.documents) {
+            this.createC100Row(document)
+
+            // Don't create item rows for receipts
+            if (![DocumentType.RECEIPT, DocumentType.DONATION_RECEIPT].includes(document.documentType)) {
+                document.items.forEach((item, index) => {
+                    this.createD110Row(document, item, index)
+                })
+            }
+            document.payments.forEach((payment, index) => {
+                this.createD120Row(document, payment, index)
+            })
+        }
+    }
+
+    private createBKMVDATAFooter() {
+        const getValue = (cell: Cell) => {
+            return (
+                this.computedValues(cell) ??
+                getNestedValue(this.input, cell.name) ??
+                cell.default
+            )
+        }
+
+        this.createRow(bkmvdataSchema.footer, getValue, undefined)
+    }
+
+    protected create() {
+        this.createBKMVDATAHeader()
+        this.createBKMVDATABody()
+        this.createBKMVDATAFooter()
+    }
 }
